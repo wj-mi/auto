c28744e7ff02c0626cd23321cfeb73b3f1f9cfeb
wj-mi <997457785@qq.com>
2017-09-21 19:09:39 -0700

    TDD auto submit

--- functional_tests.py
+++ functional_tests.py
@@ -18,12 +18,14 @@ class VisitorTest(unittest.TestCase):
         self.assertIn(u'待办事项', self.browser.title)
 
         # 页面包含一句欢迎语，邀请小明输入一个待办事项并点击提交按钮
-        self.assertIn('welcome', self.browser.context())
 
         # 小明在输入框中输入了“购买《Python核心编程》”，并点击提交按钮
-
+        input_box = self.browser.find_element_by_name('things')
+        input_box.send_keys(u"购买《Python核心编程》")
+        submit_btn = self.browser.find_element_by_id('submit_btn')
+        self.assertEqual(str(type(submit_btn)), "<class 'selenium.webdriver.firefox.webelement.FirefoxWebElement'>")
+        submit_btn.click()
         # 页面中显示了“1. 购买《Python核心编程》”
-
         # 小明在输入框中输入了“购买《测试驱动开发》”，并点击提交按钮
 
         # 页面中显示了两个待办事项
